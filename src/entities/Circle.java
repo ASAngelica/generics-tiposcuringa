e7224cd4d34a8ffafe3b7bc8e0960a9ed3c8bcb1
ASAngelica <asa196549@gmail.com>
2022-02-03 12:39:27 -0300

    Implementando Genericos tipo curinga

---
+++ src/entities/Circle.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Circle implements Shape {
+	
+	public static final double PI = 3.14159265359;
+	
+	private double radius;
+	
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return PI * radius * radius;
+	}
+
+}
